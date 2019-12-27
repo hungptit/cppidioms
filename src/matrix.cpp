75240384ea8b3d588c042a63141018abaffb69e7
Hung Dang <hungptit@gmail.com>
2019-12-27 11:25:54 -0500

    Add an example which demonstrate that we can init a matrix using one line of code.

---
+++ src/matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+template <typename T> void print(const std::vector<std::vector<T>> &mat) {
+    for (auto const &row : mat) {
+        for (auto element : row) {
+            std::cout << element << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main() {
+    std::vector<std::vector<int>> data(10, std::vector<int>{1, 2, 3, 4, 5});
+    print(data);
+    return EXIT_SUCCESS;
+}
