3018fbecb5b9f4231c4af82f75e8be6fb433ec7d
Hung Dang <hdang@tableau.com>
2020-01-29 07:55:03 -0800

    Finalize the sample code code which demnonstrate the bad usage of the shared_ptr.

--- src/misuse_smart_ptr.cpp
+++ src/misuse_smart_ptr.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <memory>
 #include <set>
 
@@ -8,16 +9,19 @@ struct Object {
     ~Object() { std::cout << "Destructor: " << data << "\n"; }
     void print(const std::string &msg) const { std::cout << msg << ":" << data << "\n"; }
     std::string data;
+    static Object* create(const std::string &data) {
+        return new Object(data);
+    }
 };
 
-int main() {
-    Object obj1("World");
-    obj1.print("Hello");
-    Object *obj2 = new Object("obj2");
-
-    {
-        std::map<int, std::unique_ptr<Object>> map;
-        map.insert(std::make_pair(0, obj2));
-    }
+void shared_ptr_case() {
+    using Map = std::map<int, std::shared_ptr<Object>>;
+    auto obj2 = Object::create("obj2");
+    Map map{{0, std::make_shared<Object>("First item")}};
+    map.insert(std::make_pair(0, obj2));
     obj2->print("Should segv");
+}
+
+int main() {
+    shared_ptr_case();
 }
