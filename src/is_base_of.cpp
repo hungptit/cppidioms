ca5d3c5ec4610505884bcc221734a90a995c2569
Hung Dang <hdang@tableau.com>
2020-06-15 18:23:50 -0700

    Add enable_if solution.

--- src/is_base_of.cpp
+++ src/is_base_of.cpp
@@ -14,8 +14,12 @@ struct C : public A {
     void print() const { std::cout << "Class C\n"; }
 };
 
-template <typename T> void exec(const T &value) {
+template <typename T> void exec_v1(const T &value) {
     static_assert(std::is_base_of<A, T>::value, "It works!");
+    value.print();
+}
+
+template <typename T, typename std::enable_if_t<std::is_base_of<A, T>::value, int> = 0> void exec_v2(const T &value) {
     value.print();
 }
 
@@ -24,9 +28,10 @@ int main() {
     B b;
     C c;
 
-    exec(a);
+    exec_v1<A>(a);
     // exec(b);
-    exec(c);
+    exec_v1(c);
+    exec_v2(c);
 
     return EXIT_SUCCESS;
 }
