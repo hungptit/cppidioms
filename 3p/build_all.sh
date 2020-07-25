445f516396e499157768545a3823dd411b28b8e7
Hung Dang <hdang@tableau.com>
2020-07-25 15:36:15 -0700

    Add Google benchmark submodule

--- 3p/build_all.sh
+++ 3p/build_all.sh
@@ -2,13 +2,13 @@
 set -e                          # Use Bash strict mode
 
 printf "Build doctest\n";
-./build_using_cmake.sh doctest "-DCMAKE_CXX_COMPILER=clang++" > /dev/null
+./build_using_cmake.sh doctest > /dev/null
 
 printf "Build FMT\n"
 ./build_using_cmake.sh fmt "-DFMT_DOC=OFF -DFMT_TEST=OFF" > /dev/null
 
 printf "Build benchmark\n"
-./build_using_cmake.sh benchmark -DBENCHMARK_ENABLE_TESTING=OFF CMAKE_CXX_COMPILER=clang++ > /dev/null
+./build_using_cmake.sh benchmark -DBENCHMARK_ENABLE_TESTING=OFF > /dev/null
 
 printf "Setup Clara\n"
 cp src/Clara/include/*.hpp include/
---
+++ src/interface_class.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+
+class Base {
+  public:
+    virtual ~Base() = default;
+    
+    void print() const { print_impl(); };
+
+  private:
+    virtual void print_impl() const { std::cout << "This is a base class\n"; }
+};
+
+class Derived : public Base {
+  public:
+    virtual ~Derived() = default;
+    
+
+  private:
+    virtual void print_impl() const { std::cout << "This is the derived class.\n"; }
+};
+
+int main() {
+    Base    base;
+    Derived derived;
+
+    base.print();
+    derived.print();
+
+    std::unique_ptr<Base> pt1 = std::make_unique<Base>();
+    pt1->print();
+
+    std::unique_ptr<Base> pt2 = std::make_unique<Derived>();
+    pt2->print();
+}
