8fc8e85eda8c05df1b5d52de72e9012eb1169bef
Hung Dang <hdang@tableau.com>
2020-08-03 06:15:45 -0700

    Add an example for getline

--- src/eigen_ops.cpp
+++ src/eigen_ops.cpp
@@ -3,24 +3,24 @@
 #include "Eigen/Dense"
 using namespace Eigen;
 int main() {
-  Matrix2d a;
-  a << 1, 2, 3, 4;
-  Vector3d v(1, 2, 3);
-  std::cout << "a * 2.5 =\n" << a * 2.5 << std::endl;
-  std::cout << "0.1 * v =\n" << 0.1 * v << std::endl;
-  std::cout << "Doing v *= 2;" << std::endl;
+  // Matrix2d a;
+  // a << 1, 2, 3, 4;
+  // Vector3d v(1, 2, 3);
+  // std::cout << "a * 2.5 =\n" << a * 2.5 << std::endl;
+  // std::cout << "0.1 * v =\n" << 0.1 * v << std::endl;
+  // std::cout << "Doing v *= 2;" << std::endl;
 
-  v *= 2.0;
-  std::cout << "Now v =\n" << v << std::endl;
+  // v *= 2.0;
+  // std::cout << "Now v =\n" << v << std::endl;
 
-  // Does not work
-  v.array() -= 2;
+  // // Does not work
+  // v.array() -= 2;
 
-  // Does xwork
-  v.array() -= 2;
-  std::cout << "Now v =\n" << v << std::endl;
+  // // Does xwork
+  // v.array() -= 2;
+  // std::cout << "Now v =\n" << v << std::endl;
 
   Vector3d x(1, 2, 3);
-  x.array().log();
-  std::cout << x << "\n";
+  x = x.array().log();
+  std::cout << "log(x): \n" << x << "\n";
 }
