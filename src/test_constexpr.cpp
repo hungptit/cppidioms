faf76a0058af8d2bd5e97516ed8b473ad567f123
Hung Dang <hungptit@gmail.com>
2020-08-12 14:32:38 -0400

    Minor update

--- src/test_constexpr.cpp
+++ src/test_constexpr.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    constexpr std::array<int, 5> x{1, 2, 3, 4, 5};
-    for (auto item : x) std::cout << item << " ";
-    std::cout << "\n";
+  constexpr std::array<int, 5> x{1, 2, 3, 4, 5};
+  for (auto item : x) std::cout << item << " ";
+  std::cout << "\n";
 }
