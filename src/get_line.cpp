9641b51d1dec938eaf0201180c76b22f4bb650ae
Hung Dang <hdang@tableau.com>
2020-09-25 09:28:07 -0700

    Add basic implementation for mean and variance.

--- src/get_line.cpp
+++ src/get_line.cpp
@@ -9,35 +9,35 @@
 #include <string>
 
 struct HyperParams {
-    explicit HyperParams(const std::string &fname) {
-        std::fstream fs;
-        fs.open(fname, std::ios::in);
-        if (!fs.is_open() || !fs.good()) {
-            const std::string errmsg = "Cannot open: " + fname;
-            throw std::runtime_error(errmsg);
-        }
-        std::string line;
-
-        std::getline(fs, line);
-        D = std::atof(line.data());
-
-        std::getline(fs, line);
-        C = std::atof(line.data());
+  explicit HyperParams(const std::string& fname) {
+    std::fstream fs;
+    fs.open(fname, std::ios::in);
+    if (!fs.is_open() || !fs.good()) {
+      const std::string errmsg = "Cannot open: " + fname;
+      throw std::runtime_error(errmsg);
     }
+    std::string line;
 
-    double D = 0.0;
-    double C = 0.0;
+    std::getline(fs, line);
+    D = std::atof(line.data());
+
+    std::getline(fs, line);
+    C = std::atof(line.data());
+  }
+
+  double D = 0.0;
+  double C = 0.0;
 };
 
-std::ostream &operator<<(std::ostream &os, const HyperParams &params) {
-    os << "{D = " << params.D << ", "
-       << "C = " << params.C << "}\n";
-    return os;
+std::ostream& operator<<(std::ostream& os, const HyperParams& params) {
+  os << "{D = " << params.D << ", "
+     << "C = " << params.C << "}\n";
+  return os;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc > 1) {
-        HyperParams params(argv[1]);
-        std::cout << "hyper-parameters: " << params << "\n";
-    }
+int main(int argc, char* argv[]) {
+  if (argc > 1) {
+    HyperParams params(argv[1]);
+    std::cout << "hyper-parameters: " << params << "\n";
+  }
 }
