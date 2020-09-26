7e01e95ddb7691535fc9022d47755c4e26f55b85
Hung Dang <hdang@tableau.com>
2020-09-25 20:35:29 -0700

    Add pmr examples

---
+++ src/pmr.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <array>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <iterator>
+#include <memory_resource>
+
+int main() {
+    std::array<char, 64> buffer;
+    std::fill_n(buffer.begin(), buffer.size(), '_');
+    std::cout << buffer.data() << '\n';
+
+    std::pmr::monotonic_buffer_resource pool{buffer.data(), buffer.size()};
+
+    std::pmr::vector<char> vec{&pool};
+
+    for (char ch = 'a'; ch <= 'z'; ++ch) vec.push_back(ch);
+
+    std::cout << buffer.data() << '\n';
+    return EXIT_SUCCESS;
+}
