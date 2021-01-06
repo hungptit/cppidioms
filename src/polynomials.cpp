6fe9324d79602b554515e03f7614e425776b55d9
Hung Dang <hdang@tableau.com>
2021-01-05 19:26:19 -0800

    Have a working version

--- src/polynomials.cpp
+++ src/polynomials.cpp
@@ -4,17 +4,16 @@
 #include <limits>
 
 int main() {
-    const double NaN = std::nan("");
-    constexpr double inf = std::numeric_limits<double>::infinity();
+  const double NaN = std::nan("");
+  constexpr double inf = std::numeric_limits<double>::infinity();
 
-    std::cout << "std::hermitel(1, -inf)" << std::hermitel(1, -inf) << "\n";
-    std::cout << "std::hermitel(1, +inf)" << std::hermitel(1, inf) << "\n";
-    std::cout << "std::hermitel(1, NaN)"<< std::hermitel(1, NaN) << "\n";
-    std::cout << "log(-inf): " << std::log(-inf) << "\n";
-    std::cout << "log(inf): " << std::log(inf) << "\n";
-    std::cout << "log(inf): " << std::log(std::numeric_limits<double>::epsilon()) << "\n";
-    std::cout << "log(inf): " << std::log(-0) << "\n";
-    std::cout << "log(inf): " << std::log(0) << "\n";
-    
-    return EXIT_SUCCESS;
+  std::cout << "std::hermitel(1, -inf)" << std::hermitel(1, -inf) << "\n";
+  std::cout << "std::hermitel(1, +inf)" << std::hermitel(1, inf) << "\n";
+  std::cout << "std::hermitel(1, NaN)" << std::hermitel(1, NaN) << "\n";
+  std::cout << "log(-inf): " << std::log(-inf) << "\n";
+  std::cout << "log(inf): " << std::log(inf) << "\n";
+  std::cout << "log(NaN): " << std::log(NaN) << "\n";
+  std::cout << "log(0): " << std::log(0.0) << "\n";
+
+  return EXIT_SUCCESS;
 }
